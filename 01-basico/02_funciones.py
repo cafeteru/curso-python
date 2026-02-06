447c83e1ba7322b9412c370b9e8503172e3928a0
Iván González <cafeteru.dev@gmail.com>
2026-02-06 13:43:53 +0100

    Funciones

---
+++ 01-basico/02_funciones.py
@@ -0,0 +1,15 @@
+def metodo_sin_argumentos():
+   print("Soy Iván")
+
+def metodo_con_argumentos(nombre: str, edad: int = 18):
+    print(f"Hola soy {nombre}")
+    print(f"y tengo {edad}")
+
+def metodo_con_return(nombre: str):
+    return nombre
+
+metodo_sin_argumentos()
+metodo_con_argumentos("Iván", 22)
+metodo_con_argumentos("Defecto")
+retorno = metodo_con_return("Retorno")
+print(retorno)
